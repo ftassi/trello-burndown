1ecb634b086a95deff3f1bb0d906b025ce679176
Francesco Tassi <tassi.francesco@gmail.com>
2016-11-01 22:28:53 +0100

    Setup react
    Define some basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack')
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public')
+var APP_DIR = path.resolve(__dirname, 'src/client/app')
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: APP_DIR + '/../index.html',
+      inject: true
+    })
+  ]
+}
+
+module.exports = config
