ba1f6fd2a3fa44eaab0bb38818cb2e5e8a894c7b
Francesco Tassi <tassi.francesco@gmail.com>
2016-11-12 15:30:22 +0100

    Add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public')
 var APP_DIR = path.resolve(__dirname, 'src/client/app')
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: ['babel-polyfill', APP_DIR + '/index.jsx'],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
